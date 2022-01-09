a126fe064b55c0233aca0876b81350a3ad2eb1b0
IIoHuMaIO <poxa2004@mail.ru>
2022-01-09 15:54:13 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw3_2/PersonViewHolder.java
@@ -0,0 +1,25 @@
+package com.example.hw3_2;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PersonViewHolder extends RecyclerView.ViewHolder {
+
+    private TextView namePersonContact;
+    private ImageView imageView;
+
+    public PersonViewHolder(@NonNull View itemView) {
+        super(itemView);
+        imageView = itemView.findViewById(R.id.buttonImage);
+        namePersonContact = itemView.findViewById(R.id.buttonText);
+    }
+
+    public void onBind(String nameContact, Integer imageIcon) {
+        imageView.setImageResource(imageIcon);
+        namePersonContact.setText(nameContact);
+    }
+}
